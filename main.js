e4df1c23ab73344935fc826db8da680331a67423
Themis <idonotwantspams@gmail.com>
2025-01-16 13:06:12 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+Hooks.on('getSceneControlButtons', (controls) => {
+	let token = controls.find((c) => c.name === 'token');
+	if (token) {
+		let i = token.tools.length;
+		token.tools.splice(i, 0, {
+			name: 'ACDC',
+			title: 'Alter Core DiceRoller Config',
+			icon: 'fa-solid fa-r',
+			visible: true,
+			onClick: changeDiceRollConfig,
+			button: true,
+		});
+	}
+});
+
+async function changeDiceRollConfig() {
+	const config = game.settings.get('core', 'diceConfiguration');
+	const isManual = game.user.getFlag('world', 'currentDiceConfig') === 'manual';
+	for (const dice in config) config[dice] = isManual ? '' : 'manual';
+	await game.settings.set('core', 'diceConfiguration', config);
+	await game.user.setFlag('world', 'currentDiceConfig', isManual ? 'auto' : 'manual');
+	ui.notifications.info(`Foundry dice rolls are now ${isManual ? 'auto' : 'manual'}`);
+}
