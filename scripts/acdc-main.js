86dbacd12de25f978c61bcea1e2927cd7f1acaf6
Themis <idonotwantspams@gmail.com>
2025-01-16 20:10:50 +0000

    Update acdc-main.mjs

--- scripts/acdc-main.js
+++ scripts/acdc-main.js
@@ -1,1 +1,26 @@
+Hooks.on('getSceneControlButtons', showTokenControlsButton);
 
+function showTokenControlsButton(controls) {
+	if (!game.permissions.MANUAL_ROLLS.includes(game.user.role)) return;
+	let token = controls.find((c) => c.name === 'token');
+	if (token) {
+		let i = token.tools.length;
+		token.tools.splice(i, 0, {
+			name: 'ACDC',
+			title: game.i18n.localize('acdc.buttonHint'),
+			icon: 'fa-solid fa-r',
+			visible: true,
+			onClick: changeDiceRollConfig,
+			button: true,
+		});
+	}
+}
+
+async function changeDiceRollConfig() {
+	const config = game.settings.get('core', 'diceConfiguration');
+	const isManual = game.user.getFlag('acdc', 'currentDiceConfig') === 'manual';
+	for (const dice in config) config[dice] = isManual ? '' : 'manual';
+	await game.settings.set('core', 'diceConfiguration', config);
+	await game.user.setFlag('acdc', 'currentDiceConfig', isManual ? 'auto' : 'manual');
+	ui.notifications.info(`Foundry dice rolls are now ${isManual ? 'auto' : 'manual'}`);
+}
