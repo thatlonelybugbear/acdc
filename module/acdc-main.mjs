01e55e219d3374483e261e113713f4861ace0f6e
Themis <idonotwantspams@gmail.com>
2025-01-17 14:40:29 +0000

    Make Button element a toggle. (#4)
    Closes #3

--- module/acdc-main.mjs
+++ module/acdc-main.mjs
@@ -2,30 +2,29 @@ Hooks.on('getSceneControlButtons', showTokenControlsButton);
 
 function showTokenControlsButton(controls) {
 	if (!game.permissions.MANUAL_ROLLS.includes(game.user.role)) return;
-	let token = controls.find((c) => c.name === 'token');
+	const active = !!getCDC(game.settings.get('core', 'diceConfiguration'));
+	const token = controls.find((c) => c.name === 'token');
 	if (token) {
-		let i = token.tools.length;
+		const i = token.tools.length;
 		token.tools.splice(i, 0, {
 			name: 'ACDC',
 			title: localize('acdc.buttonHint'),
 			icon: 'fa-solid fa-r',
 			visible: true,
+			toggle: true,
 			onClick: changeDiceRollConfig,
 			button: true,
+			active,
 		});
 	}
 }
 
 async function changeDiceRollConfig() {
-	const config = game.settings.get('core', 'diceConfiguration');
-	let initialRun = false;  //the default state is {}
-	if (foundry.utils.isEmpty(config)) {
-		initialRun = true;
-		for (const fullfillmentDice in CONFIG.Dice.fulfillment.dice) 
-			config[fullfillmentDice] = '';
-	}
-	const isManual = !initialRun && game.user.getFlag('acdc', 'currentDiceConfig') === 'manual';
-	for (const dice in config) config[dice] = isManual ? '' : 'manual';
+	const config = game.settings.get('core', 'diceConfiguration'); //the default state is {}
+	const isManual = getCDC(config);
+	if (isManual === null) {
+		for (const fullfillmentDice in CONFIG.Dice.fulfillment.dice) config[fullfillmentDice] = '';
+	} else for (const dice in config) config[dice] = isManual ? '' : 'manual';
 	await game.settings.set('core', 'diceConfiguration', config);
 	await game.user.setFlag('acdc', 'currentDiceConfig', isManual ? 'auto' : 'manual');
 	ui.notifications.info(localize(isManual ? 'acdc.auto' : 'acdc.manual'));
@@ -33,4 +32,9 @@ async function changeDiceRollConfig() {
 
 function localize(string) {
 	return game.i18n.localize(string);
+}
+
+function getCDC(config) {
+	if (foundry.utils.isEmpty(config)) return null;
+	return game.user.getFlag('acdc', 'currentDiceConfig') === 'manual';
 }
